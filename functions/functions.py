e04d18a077c6a83e40b4aecff0236e62356cc17d
Joe <98587440+nerfkidjcb@users.noreply.github.com>
2023-07-23 18:06:01 +0100

    Fixed directory referencing when checking cfg

--- functions/functions.py
+++ functions/functions.py
@@ -9,7 +9,7 @@ import customLogging as log
 
 def checkCfg():
     """Check if the configuration file exists"""
-    if not os.path.exists("../cfg/cfg.ini"):
+    if not os.path.exists("./cfg/cfg.ini"):
         log.printError("No cfg.ini file found, please create one from the example file in the cfg folder.")
         log.printError("Aborting...")
         exit()
