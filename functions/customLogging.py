28a5423dc386a49ab9a1372588df6e489519512c
Joe <nerfkidjcb@gmail.com>
2023-07-30 16:45:41 +0100

    Logs with timestamps

--- functions/customLogging.py
+++ functions/customLogging.py
@@ -1,8 +1,14 @@
+import time
+
+def getCurrentDateAndTime():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+
 def printInfo(message):
-    print("\033[94m [INFO] \033[0m" + message)
+    print("\033[94m" + getCurrentDateAndTime() + " [INFO] \033[0m" + message)
 
 def printWarn(message):
-    print("\033[93m [WARN] \033[0m" + message)
+    print("\033[93m" + getCurrentDateAndTime() + " [WARN] \033[0m" + message)
 
 def printError(message):
-    print("\033[91m [ERROR] \033[0m" + message)
+    print("\033[91m" + getCurrentDateAndTime() + " [ERROR] \033[0m" + message)
