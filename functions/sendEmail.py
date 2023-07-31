f270abcc8972941e6adebdc9328f8c3370c6c8b5
Joe <nerfkidjcb@gmail.com>
2023-07-31 15:38:31 +0100

    Email is class now

--- functions/sendEmail.py
+++ functions/sendEmail.py
@@ -1,28 +1,32 @@
 import smtplib
 import configparser
 
-# Parse cfg.ini file
-config = configparser.ConfigParser()
-config.read('./cfg/cfg.ini')
 
-# Function to email me with anything worth being notified
-def sendMail(subject, body):
+class Mail:
 
-  recipient = config['email']['email_to']
-  sender = config['email']['email_from']
-  password = config['email']['email_password']
-  message = f"Subject: {subject}\n\n{body}"
-  host= config['email']['email_host']
+  def __init__(self):      
+    # Parse cfg.ini file
+    config = configparser.ConfigParser()
+    config.read('./cfg/cfg.ini')
 
-  try:
-    server = smtplib.SMTP(host)
-    server.ehlo()
-    server.starttls()
-    server.login(sender, password)
-    server.sendmail(sender, recipient, message)
-    server.close()
-    return True
-  
-  except:
-    return False
+  # Function to email me with anything worth being notified
+  def sendMail(subject, body):
+
+    recipient = config['email']['email_to']
+    sender = config['email']['email_from']
+    password = config['email']['email_password']
+    message = f"Subject: {subject}\n\n{body}"
+    host= config['email']['email_host']
+
+    try:
+      server = smtplib.SMTP(host)
+      server.ehlo()
+      server.starttls()
+      server.login(sender, password)
+      server.sendmail(sender, recipient, message)
+      server.close()
+      return True
+    
+    except:
+      return False
 
